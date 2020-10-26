f797ce6e5f19a06643749ff1358c5df7cb356c93
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-10-26 12:45:06 -0400

    updated to output in readable format

--- scrape.py
+++ scrape.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bsoup
-import pprint
 
 site = 'https://news.ycombinator.com/news'
 query_param = '?p='
 num_of_pages = int(
     input('Enter the number of pages you would like to scrape: '))
+popularity = int(input('Enter the number of votes required for a post: '))
 links = []
 subtext = []
 
@@ -15,6 +15,7 @@ for page in range(1, num_of_pages+1):
     new_soup = bsoup(res.text, 'html.parser')
     links += new_soup.select('.storylink')
     subtext += new_soup.select('.subtext')
+
 
 def sort_by_votes(hnlist):
     # whenever sorting with dictionaries
@@ -29,9 +30,14 @@ def create_custom_hn(links, subtext):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            if points > 99:
+            if points >= popularity:
                 hn.append({'title': title, 'link': href, 'votes': points})
     return sort_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+stories = create_custom_hn(links, subtext)
+
+for story in enumerate(stories):
+    print('Title:', story[1]['title'])
+    print('Link to article:', story[1]['link'])
+    print('Votes:', story[1]['votes'], '\n')
